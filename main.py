d41694f557dbb777fb204189bfd989070c23d860
Jeffrey Tan <tanjeffreyz02@berkeley.edu>
2022-05-21 15:44:39 -0700

    table of contents working, navbar also working

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import asyncio
 import urllib.error
 from urllib.request import urlopen
 from client import Client
+from readme import Readme
 
 
 class Main:
@@ -14,7 +15,9 @@ class Main:
 
     async def run(self):
         repos = await self.get_all_repos()
-        print(self.filter_repos(repos))
+        filtered = self.filter_repos(repos)
+        readme = Readme(filtered)
+        readme.generate()
 
     async def get_all_repos(self):
         """Returns all public repositories the user contributed to."""
@@ -49,24 +52,24 @@ class Main:
     def filter_repos(self, repos):
         """Filters repositories and keeps those with tags in SELF.TAGS"""
 
+        print('\n[~] Parsing repositories:')
         filtered = {}
         for r in repos:
             name = r.get('name', '')
             owner = r.get('owner', {}).get('login', '')
             default_branch = r.get('defaultBranchRef', {}).get('name', '')
-            print(name, owner, default_branch)
             url = f'https://raw.githubusercontent.com/{owner}/{name}/{default_branch}/README.md'
             try:
                 contents = [x.decode('utf-8').strip() for x in urlopen(url)]
                 header = parse_header(contents)
                 if header is not None and any(x in header['tags'] for x in self.TAGS):
-                    r['tags'] = header['tags']
+                    r['header'] = header
                     category = header['category']
                     if category not in filtered:
                         filtered[category] = []
                     filtered[category].append(r)
             except urllib.error.HTTPError:
-                print(f"\n[!] Invalid URL: '{url}'")
+                print(f" !  Invalid URL: '{url}'")
         return filtered
 
     async def get_repos_page(self, owned_cursor, contrib_cursor):
@@ -91,6 +94,8 @@ def parse_header(contents):
     # Parse header for repository information
     header = {
         'tags': None,
+        'title': None,
+        'authors': None,
         'category': None
     }
     for line in contents[start:end]:
@@ -98,10 +103,12 @@ def parse_header(contents):
         if len(args) == 2:
             key = args[0].strip().lower()
             value = args[1].strip()
-            if key == 'tags':
-                header['tags'] = {x.strip().lower() for x in value.split(',')}
-            elif key == 'category':
-                header['category'] = value
+            if key in {'tags'}:
+                header[key] = {x.strip().lower() for x in value.split(',')}
+            elif key in {'category', 'title'}:
+                header[key] = value
+            elif key in {'authors'}:
+                header[key] = [x.strip() for x in value.split(',')]
     if all(x is not None for x in header.values()):
         return header
 
---
+++ readme.py
@@ -0,0 +1,91 @@
+import os
+
+
+class Readme:
+    def __init__(self, repos):
+        self.REPOS = repos
+
+    def generate(self):
+        CATEGORIES = sorted(self.REPOS.keys())
+
+        contents = []
+
+        index_start, index_end = load_template('index')
+        contents += index_start
+
+        # Navigation bar
+        navbar_start, navbar_end = load_template('navbar')
+        contents += navbar_start
+        for key in CATEGORIES:
+            anchor = get_anchor(key) + '-category'
+            contents.append(f'<li><a class="dropdown-item" href="#{anchor}">{key}</a></li>')
+        contents += navbar_end
+
+        # Title
+        contents += load_template('title')[0]
+
+        # Table of Contents
+        toc_start, toc_end = load_template('toc')
+        contents += toc_start
+        for i, key in enumerate(CATEGORIES):
+            classes = 'list-group' + (' mt-3' if i != 0 else '')
+            contents += [
+                f'<ul class="{classes}">',
+                f'<li class="list-group-item list-group-item-primary">{key}</li>'
+            ]
+            for repo in self.REPOS[key]:
+                anchor = get_anchor(repo.get('name', ''))
+                title = repo.get('header', {}).get('title', '')
+                contents.append(f'<li class="list-group-item">'
+                                f'<a class="text-decoration-none ms-4" href="#{anchor}">{title}</a></li>')
+            contents.append('</ul>')
+        contents += toc_end
+
+        # Repositories
+
+        contents += index_end       # Finish index
+
+        # Add indentation
+        indent = 0
+        for i in range(len(contents)):
+            line = contents[i]
+            next_indent = indent
+            if all(x not in line for x in ('meta', 'link', '!DOCTYPE')):
+                first = True
+                for j in range(len(line)):
+                    if line[j] == '<':
+                        if j < len(line) - 1 and line[j+1] == '/':
+                            if first:
+                                indent -= 1         # If '</' comes first, decrease current indent
+                            next_indent -= 1
+                        else:
+                            next_indent += 1
+                        first = False
+            contents[i] = ' ' * 4 * max(0, indent) + line
+            indent = next_indent
+
+        # Save to file
+        contents.append('')
+        with open('index.html', 'w') as file:
+            file.write('\n'.join(contents))
+
+
+def get_anchor(string):
+    return string.lower().replace(' ', '-')
+
+
+def load_template(name):
+    blocks = []
+    lines = []
+    with open(os.path.join('resources', 'templates', f'{name}.txt')) as file:
+        for line in file.readlines():
+            line = line.strip()
+            if line != '__DIVIDER__':
+                if line:
+                    lines.append(line)
+            else:
+                blocks.append(lines)
+                lines = []
+    if len(lines) > 0:
+        blocks.append(lines)
+    return blocks
